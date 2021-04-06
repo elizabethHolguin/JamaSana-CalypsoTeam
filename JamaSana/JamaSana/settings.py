5b6de4f92cd0a2a3bbfc3e5c50c4665bdb1334d2
Marlon-Lindao-ZX <melindao@espol.edu.ec>
2021-04-06 12:51:22 -0500

    Añadiendo path para imagenes, de tal manera se puedan cargar

--- JamaSana/JamaSana/settings.py
+++ JamaSana/JamaSana/settings.py
@@ -144,6 +144,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
+
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- JamaSana/productos/models.py
+++ JamaSana/productos/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 from django.forms import ModelForm
+
+import os
+from django.dispatch import receiver
+
+def get_upload_to_categoria(instance, filename):
+    folder_name = 'categoria'
+    #print(instance.hueca_id)
+    return os.path.join(folder_name, filename)
+
+def get_upload_to_comida(instance, filename):
+    folder_name = 'comida'
+    #print(instance.hueca_id)
+    return os.path.join(folder_name, filename)
+
 # Create your models here.
 class Categoria(models.Model):
     nombre = models.CharField(max_length=200)
-    imagen = models.ImageField(null=True,blank=True)
+    imagen = models.ImageField(null=True,blank=True,upload_to=get_upload_to_categoria)
 
     class CategoriaForm(ModelForm):
         class Meta:
@@ -13,14 +27,45 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombre
 
+@receiver(models.signals.post_delete, sender=Categoria)
+def auto_delete_file_on_delete_Categoria(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.imagen:
+        if os.path.isfile(instance.imagen.path):
+            os.remove(instance.imagen.path)
+
+
+@receiver(models.signals.pre_save, sender=Categoria)
+def auto_delete_file_on_change_Categoria(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem
+    when corresponding `MediaFile` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = sender.objects.get(pk=instance.pk).imagen
+    except sender.DoesNotExist:
+        return False
+
+    new_file = instance.imagen
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
+
 class Comidas(models.Model):
     nombre =  models.CharField(max_length=200)
     descripcion = models.CharField(max_length=200)
     macronutrientes = models.CharField(max_length=1000)
     estado = models.BooleanField(default=True)
     precio = models.FloatField(default=0)
-    calorias_totales = models.FloatField()
-    imagen = models.ImageField()
+    calorias_totales = models.FloatField(default=0)
+    imagen = models.ImageField(upload_to=get_upload_to_comida)
     id_categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
     id_vendedor = models.ForeignKey("usuarios.Vendedor", on_delete=models.DO_NOTHING)
 
@@ -33,6 +78,36 @@ class Comidas(models.Model):
         return self.nombre + ' - ' + self.descripcion
 
 
+@receiver(models.signals.post_delete, sender=Comidas)
+def auto_delete_file_on_delete_Categoria(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.imagen:
+        if os.path.isfile(instance.imagen.path):
+            os.remove(instance.imagen.path)
+
+
+@receiver(models.signals.pre_save, sender=Comidas)
+def auto_delete_file_on_change_Categoria(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem
+    when corresponding `MediaFile` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = sender.objects.get(pk=instance.pk).imagen
+    except sender.DoesNotExist:
+        return False
+
+    new_file = instance.imagen
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
 
 class Pedido(models.Model):
     id_cliente = models.ForeignKey("usuarios.Cliente", on_delete=models.CASCADE)
