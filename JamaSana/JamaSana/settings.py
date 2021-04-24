fda0d3d80295d6bbf99884a70f3edb088697c3c4
Dayse Maroto L <50672339+dmaroto98@users.noreply.github.com>
2021-04-24 13:43:09 -0500

    Update settings.py
    agregando token authorization in settings

--- JamaSana/JamaSana/settings.py
+++ JamaSana/JamaSana/settings.py
@@ -91,6 +91,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
 }
 
